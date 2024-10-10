8ce0904926575444d3c3a5cd2ed0ff9dbc522d42
Manideep Mavilapalli <manideepmavilapalli@Manideeps-MacBook-Pro.local>
2024-10-10 11:50:29 +0530

    m

--- labs/1-Initial-Setup/scripts/create-terraform-storage.sh
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-RESOURCE_GROUP_NAME="devops-journey-rg"
-STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+RESOURCE_GROUP_NAME="Bobby-group"
+STORAGE_ACCOUNT_NAME="azuredevops"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l centralindia -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l centralindia --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
